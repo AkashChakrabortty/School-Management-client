5cb8fd8d9e744c46de07cbcbf1636a6c59b361f1
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-04 18:05:23 +0600

    carousel add

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from '../Carousel/Carousel';
+
+const Home = () => {
+    return (
+      <div>
+        <h1 className='text-2xl font-bold text-center'>Bhitabaria Model School</h1>
+        <Carousel></Carousel>
+      </div>
+    );
+};
+
+export default Home;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import Footer from '../components/Footer/Footer';
+import Home from '../components/Home/Home';
 import NavBar from '../components/NavBar/NavBar';
 import CheckinStudent from '../Pages/CheckinStudent/CheckinStudent';
 import ErrorPage from '../Pages/ErrorPage/ErrorPage';
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
     ),
     errorElement: <ErrorPage></ErrorPage>,
     children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
       {
         path: "/checkin",
         element: <CheckinStudent></CheckinStudent>,
