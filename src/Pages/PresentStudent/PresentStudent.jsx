b523ac40ba43ba3d9da40747c65022a24aa6d68a
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-05 20:15:45 +0600

    checkout

--- src/Pages/PresentStudent/PresentStudent.jsx
+++ src/Pages/PresentStudent/PresentStudent.jsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from "react";
 
 const PresentStudent = () => {
   const [students, setStudents] = useState([]);
+  const [reFetch,setReFetch] = useState(false)
   useEffect(() => {
     fetch(`http://localhost:5000/getPresentStudent`)
       .then((res) => res.json())
       .then((data) => {
         setStudents(data);
       });
-  }, []); 
+  }, [reFetch]); 
+  const handleCheckOut = (student) => {
+ fetch(`http://localhost:5000/CheckOut/${student._id}`, {
+   method: "DELETE"})
+   .then((res) => res.json())
+   .then((data) => {
+    setReFetch(!reFetch)
+   });
+  }
   return (
     <div>
       <h2 className="text-center font-bold text-xl">
@@ -21,23 +30,23 @@ const PresentStudent = () => {
             <tr>
               <th>Name</th>
               <th>Roll</th>
+              <th>Checkin time</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {
-            students.map(student => {
+            {students.map((student) => {
               return (
                 <tr>
                   <td>{student.name}</td>
                   <td>{student.roll}</td>
+                  <td>{new Date(student.checkinTime).toUTCString()}</td>
                   <td>
-                    <button className="btn">CheckOut</button>
+                    <button className="btn" onClick={()=>handleCheckOut(student)}>CheckOut</button>
                   </td>
                 </tr>
               );
-            })
-            }
+            })}
           </tbody>
         </table>
       </div>
