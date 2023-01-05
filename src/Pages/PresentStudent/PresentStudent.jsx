84650a113f71e5e557e814097fa3418f416c2097
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-05 19:57:01 +0600

    see all present stu

--- src/Pages/PresentStudent/PresentStudent.jsx
+++ src/Pages/PresentStudent/PresentStudent.jsx
@@ -1,49 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const PresentStudent = () => {
+  const [students, setStudents] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/getPresentStudent`)
+      .then((res) => res.json())
+      .then((data) => {
+        setStudents(data);
+      });
+  }, []); 
   return (
     <div>
       <h2 className="text-center font-bold text-xl">
-        Total present student right now:3
+        Total present student right now:{students.length}
       </h2>
 
       <div className="overflow-x-auto">
         <table className="w-11/12 mx-auto text-center border border-primary">
           <thead>
             <tr>
-              <th></th>
               <th>Name</th>
               <th>Roll</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>
-                <button className="btn">CheckOut</button>
-              </td>
-            </tr>
-
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>
-                <button className="btn">CheckOut</button>
-              </td>
-            </tr>
-
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>
-                <button className="btn">CheckOut</button>
-              </td>
-            </tr>
+            {
+            students.map(student => {
+              return (
+                <tr>
+                  <td>{student.name}</td>
+                  <td>{student.roll}</td>
+                  <td>
+                    <button className="btn">CheckOut</button>
+                  </td>
+                </tr>
+              );
+            })
+            }
           </tbody>
         </table>
       </div>
