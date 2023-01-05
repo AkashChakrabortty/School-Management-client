506729b388945552b1dd993d7500fad59393c2c8
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-01-05 19:45:08 +0600

    insert present student

--- src/Pages/CheckinStudent/CheckinStudent.jsx
+++ src/Pages/CheckinStudent/CheckinStudent.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
-
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const CheckinStudent = () => {
+  const notify = () => toast("Checkin Student Success!");
+  const handleForm = (event) => {
+  event.preventDefault();
+  const name = event.target.name.value;
+  const roll = event.target.roll.value;
+  const checkinTime = new Date().getTime();
+  const studentInfo = {
+    name,
+    roll,
+    checkinTime,
+  };
+  fetch("http://localhost:5000/CheckinStudent", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(studentInfo),
+  })
+    .then((res) => res.json())
+     .then((data) => {
+      notify()
+       event.target.reset();
+    });
+  }
     return (
       <div>
         <div className="w-1/2 mx-auto">
           <h2 className="text-center text-2xl">Checkin Student</h2>
-          <form className="border border-primary p-2 rounded text-center my-2">
+          <form
+            className="border border-primary p-2 rounded text-center my-2"
+            onSubmit={handleForm}
+          >
             <div className="name">
               <label>Enter Student Name:</label> <br />
               <input
                 type="text"
+                name="name"
                 placeholder="Enter Student Name......."
                 className="input input-bordered w-full max-w-xs text-primary"
               />
@@ -18,15 +47,19 @@ const CheckinStudent = () => {
               <label>Enter Student Roll Number:</label> <br />
               <input
                 type="number"
+                name="roll"
                 placeholder="Enter Student Roll Number......"
                 className="input input-bordered w-full max-w-xs text-primary"
               />
             </div>
             <div className="text-center my-2">
-              <button className="btn btn-outline btn-accent">Checkin</button>
+              <button className="btn btn-outline btn-accent" type="submit">
+                Checkin
+              </button>
             </div>
           </form>
         </div>
+        <ToastContainer />
       </div>
     );
 };
